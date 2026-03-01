8b905e9a74ba92f50ffc908329f751c3b914bfc6
p-cap <slashedeye@gmail.com>
2026-02-28 19:09:57 -0600

    removed BugSweeper for now

--- BugSweeperDemo/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace BugSweeperDemo;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
